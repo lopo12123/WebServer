fb9be5e86095d2529eb2ffc99c6cc385bbcca828
chenmh <2036604698@qq.com>
2022-06-08 16:09:23 +0800

    ...

--- server.js
+++ server.js
@@ -34,18 +34,25 @@ app.use(session({
 }))
 
 // Access the session as req.session
-app.get('/', function (req, res, next) {
-    if (req.session.views) {
-        req.session.views++
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<p>views: ' + req.session.views + '</p>')
-        res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-        res.end()
-    }
-    else {
-        req.session.views = 1
-        res.end('welcome to the session demo. refresh!')
-    }
+// app.get('/', (req, res, next) => {
+//     if (req.session.views) {
+//         req.session.views++
+//         res.setHeader('Content-Type', 'text/html')
+//         res.write('<p>views: ' + req.session.views + '</p>')
+//         res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+//         res.end()
+//     }
+//     else {
+//         req.session.views = 1
+//         res.end('welcome to the session demo. refresh!')
+//     }
+// })
+
+app.get('/', (req, res) => {
+    res
+        .cookie('env1', 'private')
+        .cookie('env2', 'httpOnlyTest')
+        .send('index.html')
 })
 
 app.get('/redirect', (req, res) => {
